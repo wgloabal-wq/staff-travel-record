546e3c7afb1801763be7514ec51472ca8dc67903
tarunkumar3713-max <tarunkumar3713@gmail.com>
2026-09-22 12:15:02 +0530

    Update login branding and vertical logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -104,7 +104,7 @@ export default function LoginPage() {
               <img
                 src="/vertical-logo.png"
                 alt="World Global Manpower Pvt. Ltd."
-                className="h-auto w-[420px] object-contain"
+                className="h-auto w-[840px] object-contain"
               />
             </div>
 
